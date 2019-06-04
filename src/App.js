53c735ff9db42ee9d0fe5dceca9fe4b49775bbfd
Jimin <jiminyun7@gmail.com>
2019-06-04 19:05:53 -0400

    router setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-class App extends Component {
-  render() {
-    return <h1>hello from tech store</h1>;
-  }
-}
+
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import SingleProduct from "./pages/SingleProduct";
+import Products from "./pages/Products";
+import About from "./pages/About";
+import Default from "./pages/Default";
+
+import { Route, Switch } from "react-router-dom";
+
+const App = () => {
+  return (
+    <>
+      {/* navbar, sidebar */}
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/cart" component={Cart} />
+        <Route path="/product/:id" component={SingleProduct} />
+        <Route path="/products" component={Products} />
+        <Route path="/about" component={About} />
+        <Route component={Default} />
+      </Switch>
+    </>
+  );
+};
 
 export default App;
